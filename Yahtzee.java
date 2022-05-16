43d2c81b92add528f6a0c330a84044bc8a7ff341
nwelsh <71775175+nwelsh@users.noreply.github.com>
2022-05-16 14:41:44 -0500

    Update Yahtzee.java

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,14 +1,26 @@
-import java.util.Scanner;
+import java.util.Random;
 
 
 public class Yahtzee {
 
   public static void main(String[] args) {
-    Scanner userIn = new Scanner(System.in);
-    System.out.println("Enter something: ");
+    Random randNum = new Random();
+    //
+    // Scanner userIn = new Scanner(System.in);
+    // System.out.println("Enter something: ");
+    //
+    // String in = userIn.nextLine();
+    // System.out.println("input: " + in);
 
-    String in = userIn.nextLine();
-    System.out.println("input: " + in);
+
+
+    int die1 = randNum.nextInt(6) + 1;
+    int die2 = randNum.nextInt(6) + 1;
+    int die3 = randNum.nextInt(6) + 1;
+    int die4 = randNum.nextInt(6) + 1;
+    int die5 = randNum.nextInt(6) + 1;
+    System.out.println("your roll: " + die1 + " " + die2 + " " + die3 + " " + die4 + " " + die5);
+
 
   }
 
