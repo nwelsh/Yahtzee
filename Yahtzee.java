61a52acdb8935bc67ab2c56fbfd140567e468d08
nwelsh <71775175+nwelsh@users.noreply.github.com>
2022-05-16 14:23:14 -0500

    Add files via upload

---
+++ Yahtzee.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Yahtzee {
+
+  public static void main(String[] args) {
+    Scanner userIn = new Scanner(System.in);
+    System.out.println("Enter something: ");
+
+    String in = userIn.nextLine();
+    System.out.println("input: " + in);
+
+  }
+
+}
